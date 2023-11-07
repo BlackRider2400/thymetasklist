dac289ebae8a2ea8640043d7c8b396c96c01f0b8
BlackRider2400 <krzysztof@baralkiewicz.pl>
2023-11-07 23:25:33 +0100

    first commit

---
+++ src/main/java/com/xdstudios/thymetasklist/ThymeTaskListApplication.java
@@ -0,0 +1,13 @@
+package com.xdstudios.thymetasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeTaskListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeTaskListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xdstudios/thymetasklist/ThymeTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdstudios.thymetasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeTaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
